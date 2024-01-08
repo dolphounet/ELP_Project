adde10de169d4929db2f1da3ac309a1bcfdc9e2a
MadMuses <mamusique128@gmail.com>
2024-01-08 17:18:58 +0100

    ImageFiltering tested and functionnal

--- ImageFiltering.go
+++ ImageFiltering.go
@@ -1,15 +1,27 @@
-package filtering
+package main
 
-func filtering() {
+import "fmt"
+
+func main() {
+	a := [][]float64{
+		{5, 1, 5, 3},
+		{4, 5, 7, 7},
+	}
+	a = resize(a, 2, 4)
+	suppressNonMax(a, 4, 6, 7)
+	contouring(a, 4, 6)
+	for i := range a {
+		fmt.Println(a[i])
+	}
 
 }
 
 func suppressNonMax(matrix [][]float64, height int, width int, max float64) {
-	const tolerance float64 = 0.0
+	const tolerance float64 = 3
 
 	for i := 0; i < height; i++ {
 		for j := 0; j < width; j++ {
-			if matrix[i][j] < max-tolerance {
+			if matrix[i][j] < (max - tolerance) {
 				matrix[i][j] = 0
 			}
 		}
@@ -18,32 +30,41 @@ func suppressNonMax(matrix [][]float64, height int, width int, max float64) {
 
 func contouring(matrix [][]float64, height int, width int) {
 
-	const lowerThreshold float64 = 0.1
-	const highThreshold float64 = 0.3
+	const lowerThreshold float64 = 4
+	const highThreshold float64 = 6
 
 	for i := 0; i < height; i++ {
 		for j := 0; j < width; j++ {
-			if matrix[i][j] < 0.1 {
+			if matrix[i][j] < lowerThreshold {
 				matrix[i][j] = 0
-
-			}
-			if matrix[i][j] > 0.3 {
+			} else if matrix[i][j] > highThreshold {
 				matrix[i][j] = 255
-
+			} else {
+				for k := -1; k <= 1; k++ {
+					for l := -1; l <= 1; l++ {
+						if matrix[i+k][j+l] > highThreshold {
+							matrix[i][j] = 255
+							matrix[i+k][j+l] = 255
+						}
+					}
+				}
+				if matrix[i][j] != 255 {
+					matrix[i][j] = 0
+				}
 			}
 		}
 	}
 }
 
-func resize(matrix [][]float64, height int, width int) {
-	line := make([]float64, width)
-	for k := 0; k < width; k++ {
-		line = append(line, 0)
-	}
+func resize(matrix [][]float64, height int, width int) [][]float64 {
+	line := make([]float64, width+2)
+
 	for i := 0; i < height; i++ {
 		matrix[i] = append(matrix[i], 0)
 		matrix[i] = append([]float64{0}, matrix[i]...)
 	}
 	matrix = append(matrix, [][]float64{line}...)
 	matrix = append([][]float64{line}, matrix...)
+
+	return matrix
 }
