f492bf52427b07c7173e84e9d2a35faa329b5879
MadMuses <mamusique128@gmail.com>
2024-01-22 17:16:43 +0100

    Can decode Json

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -5,7 +5,8 @@ import Html exposing (Html, Attribute, text, pre, div, input)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 import Http
-
+import Json.Decode as JD exposing (Decoder, map2, field, string)
+import Browser.Dom exposing (Element)
 
 
 -- MAIN
@@ -27,23 +28,39 @@ type Status
   = Failure
   | Loading
   | Success String
+  | SucessDef (List Def)
 
 type alias Model
   = { file : Status
   , userWord : String 
   , word : String}
 
+type alias Def =
+  { word : String
+  , meanings : List Meaning
+  }
 
+type alias Meaning =
+  { partOfSpeech : String
+  , definitions : List String
+  }
+
+{--
+type alias Definition =
+  { definition : String
+  , example : String
+  }
+--}
 init : () -> (Model, Cmd Msg)
 init _ =
   ( {file = Loading, userWord = "", word = "any"}
-  , Http.get
+  , getRandomDef)
+
+getWordList : Cmd Msg
+getWordList = 
+  Http.get
       { url = "https://raw.githubusercontent.com/dolphounet/ELP_Project/main/elm/thousand_words_things_explainer.txt"
-      , expect = Http.expectString GotText
-      }
-  )
-
-
+      , expect = Http.expectString GotText}
 
 -- UPDATE
 
@@ -51,6 +68,7 @@ init _ =
 type Msg
   = GotText (Result Http.Error String) 
   | ChangeInput String
+  | GotDef (Result Http.Error (List Def))
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -65,6 +83,13 @@ update msg model =
     ChangeInput word -> 
       ({model | userWord = word}, Cmd.none)
 
+    GotDef result ->
+      case result of
+        Ok def ->
+          ({model | file = SucessDef def}, Cmd.none)
+
+        Err _ ->
+          ({model | file = Failure}, Cmd.none)
 
 
 -- SUBSCRIPTIONS
@@ -73,6 +98,10 @@ update msg model =
 subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
+
+
+--Visuals
+
 
 
 
@@ -89,12 +118,41 @@ view model =
     Loading ->
       text "Loading..."
 
+    SucessDef def ->
+      div [] [text "Michel"]
+
     Success fullText ->
       pre [] [ text fullText ]
     ]
     
-  , div [] [ if model.userWord == model.word then text "You guessed it !" else if model.userWord == "" then text "Type in your guess" else text "Try again"]
-  , div [] [ input [ placeholder "Type your guess", value model.userWord, onInput ChangeInput ] [] ] 
-  , div [] [ input [type_ "checkbox"] []  , text "Show the solution"] ]
+    , div [] [ if model.userWord == model.word then text "You guessed it !" else if model.userWord == "" then text "Type in your guess" else text "Try again"]
+    , div [] [ input [ placeholder "Type your guess", value model.userWord, onInput ChangeInput ] [] ] 
+    , div [] [ input [type_ "checkbox"] []  , text "Show the solution"] ]
+
+    
+
+-- HTTP
 
 
+getRandomDef : Cmd Msg
+getRandomDef =
+  Http.get
+    { url = "https://api.dictionaryapi.dev/api/v2/entries/en/hello"
+    , expect = Http.expectJson GotDef wordDecoder
+    }
+
+wordDecoder : Decoder (List Def)
+wordDecoder =
+  JD.list defDecoder
+
+defDecoder : Decoder Def
+defDecoder =
+  map2 Def
+    (field "word" string)
+    (field "meanings" (JD.list meaningDecoder))
+
+meaningDecoder : Decoder Meaning
+meaningDecoder =
+  map2 Meaning
+    (field "partOfSpeech" string)
+    (field "definitions" (JD.list (field "definition" string)))
