7ce834521f4e1302ba361f684a8a74832182a8e8
dolphounet <maxencepj.04@gmail.com>
2024-01-26 10:34:46 +0100

    log function works

--- js/jarnac.js
+++ js/jarnac.js
@@ -1,4 +1,4 @@
-let sac = [14, 4, 7, 5, 19, 2, 4, 2, 11, 1, 1, 6, 5, 9, 8, 4, 1, 10, 7, 9, 8, 2, 1, 1, 1, 2];
+const { writeFile, appendFile } = require('node:fs/promises');
 
 function draw(sac, n) {
   let letters = [];
@@ -13,7 +13,41 @@ function draw(sac, n) {
   }
   return letters
 }
+
+function fileInit(file) {
+  try {
+    writeFile(file, "");
+    console.log("c'est bongue");
+  } catch (error) {
+    console.log("erreur", error.message);
+  };
+}
+
+function log(file, data) {
+  try {
+    appendFile(file, data+"\n\r");
+  } catch (error) {
+    console.log("error", error.message)
+  }
+}
+
+let sac = [14, 4, 7, 5, 19, 2, 4, 2, 11, 1, 1, 6, 5, 9, 8, 4, 1, 10, 7, 9, 8, 2, 1, 1, 1, 2];
 let carpet = draw(sac, 5)
 console.log(carpet)
 console.log(sac)
 console.log(carpet.map((letter) => String.fromCharCode(letter + 65)))
+
+
+fileInit('log')
+
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+
+const rl = readline.createInterface({ input, output });
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  log('log', answer)
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
