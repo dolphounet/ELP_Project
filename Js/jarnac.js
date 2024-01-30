bdb8c325705fb90abe8d6991a8b891f4006f0186
Madmuses <mamusique128@gmail.com>
2024-01-30 10:10:16 +0100

    added game init,started game function

--- Js/jarnac.js
+++ Js/jarnac.js
@@ -31,13 +31,70 @@ function log(file, data) {
   }
 }
 
-let sac = [14, 4, 7, 5, 19, 2, 4, 2, 11, 1, 1, 6, 5, 9, 8, 4, 1, 10, 7, 9, 8, 2, 1, 1, 1, 2];
-let carpet = draw(sac, 5)
-console.log(carpet)
-console.log(sac)
-console.log(carpet.map((letter) => String.fromCharCode(letter + 65)))
+function anagram(newWord,word,carpet) {
+  // Initialisation
+  const res = true
+  newWord = newWord.toUpperCase()
+  word = word.toUpperCase()
 
+  // Test de la présence du mots dans la nouvelle propal
+  for (let i = 0; i < word.length; i++) {
+    if (!newWord.includes(word[i])){
+      res = false
+    };
+  }
+  // Test si les lettres restantes sont sur le tapis
+  remains = newWord.replace(word,'')
+  for(let i=0;i<6;i++) {
+    const letter = carpet[i];
+    if (remains.includes(letter)) {remains = remains.replace(letter,'')}
+  }
+  if (remains === '') {res = true}
+  
+  return res
+}
 
+function initGame(){
+  // Creation des objets de jeu
+  let sac = [14, 4, 7, 5, 19, 2, 4, 2, 11, 1, 1, 6, 5, 9, 8, 4, 1, 10, 7, 9, 8, 2, 1, 1, 1, 2];
+  let tour = 0;
+  let carpets = [draw(sac, 6),draw(sac,6)];
+  let grilles = [[],[]];
+  for (let i = 0; i < 2; i++){
+    let grilleA = ["","","","","","","",""];
+    let grilleB = ["","","","","","","",""];
+  }
+
+  return {tour,sac,carpets,grilles}
+}
+
+function game(){
+  // Initialisation du jeu
+  gameValues = initGame()
+  let tour = gameValues.tour
+  let sac = gameValues.sac
+  let carpets = gameValues.carpets
+  let grilles = gameValues.grilles
+  let tourEnCours = 0
+
+  // Affichage de début de jeu
+  console.log("Let's begin")
+
+  // Boucle de jeu
+  while (true){
+    if (tour%2==0){
+      console.log("aoiadzjçaodaçadnabd  anoiadnaoiazhuazodbdoabaz")
+    }
+  }
+}
+
+console.log("carpet")
+//console.log(carpet)
+//console.log(sac)
+//console.log(carpet.map((letter) => String.fromCharCode(letter + 65)))
+//console.log([19,18,20,17,21].map((letter) => String.fromCharCode(letter + 65)))
+//console.log(anagram("ruat","rat",[19,18,20,17,21].map((letter) => String.fromCharCode(letter + 65))))
+game()
 fileInit('log')
 
 const readline = require('node:readline');
