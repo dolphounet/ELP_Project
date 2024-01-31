83bd354b11b4e465bcedaebdbab9eab5cea9bee0
dolphounet <maxencepj.04@gmail.com>
2024-01-31 15:48:58 +0100

    added uppercased directories (the good ones)

---
+++ GO/image_filter.go
@@ -0,0 +1,286 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"log"
+	"math"
+	"os"
+	"sync"
+	"time"
+)
+
+func generateGaussianFilter(size int, sigma float64) [][]float64 {
+	filter := make([][]float64, size)
+	center := size / 2
+	sum := float64(0)
+
+	for i := 0; i < size; i++ {
+		filter[i] = make([]float64, size)
+		for j := 0; j < size; j++ {
+			x := float64(i - center)
+			y := float64(j - center)
+			filter[i][j] = float64(math.Exp(float64(-(x*x+y*y)/(2*sigma*sigma)))) / (2 * float64(math.Pi) * sigma * sigma)
+			sum += filter[i][j]
+		}
+	}
+
+	// Normalize the filter
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			filter[i][j] /= sum
+		}
+	}
+
+	return filter
+}
+
+func convolveParal(img [][]float64, imgRes [][]float64, kernel [][]float64, width_deb, width_fin int, wg *sync.WaitGroup){
+  height := len(img)
+  width := len(img[0])
+  for y := 0; y<height; y++{
+    for x := width_deb; x<width_fin; x++{
+      sum := float64(0)
+      for i := 0; i<len(kernel); i++{
+        for j := 0; j<len(kernel); j++{
+          if x + i - len(kernel)/2 >= 0 && y + j - len(kernel)/2 >= 0 && x+i -len(kernel)/2 < width && y+j-len(kernel)/2<height{
+            sum += img[y+j-len(kernel)/2][x+i-len(kernel)/2]*kernel[i][j]
+          }
+        }
+      }
+      imgRes[y][x] = sum
+    }
+  }
+  defer wg.Done()
+}
+
+func sobel(img [][]float64, result [][]float64, width_deb, width_fin int, maxChan []float64, wg *sync.WaitGroup) {
+  var wg_sobel sync.WaitGroup
+  x_sobel := [][]float64 {
+    {1, 0, -1},
+    {2, 0, -2},
+    {1, 0, -1},
+  }
+  y_sobel := [][]float64 {
+    {1, 2, 1},
+    {0, 0, 0},
+    {-1, -2, -1},
+  }
+  width := len(img[0])
+	height := len(img)
+  sumX := make([][]float64, height)
+  sumY := make([][]float64, height)
+  for i := 0; i < height; i++ {
+    sumX[i] = make([]float64, width)
+    sumY[i] = make([]float64, width)
+  } 
+  for i := float64(0); i < 2; i++ {
+    wg_sobel.Add(2)
+    go convolveParal(img, sumX, x_sobel, width_deb+int(float64(width_fin-width_deb)*(i/2)),width_deb+int(float64(width_fin-width_deb)*((i+1)/2)), &wg_sobel)
+    go convolveParal(img, sumY, y_sobel, width_deb+int(float64(width_fin-width_deb)*(i/2)),width_deb+int(float64(width_fin-width_deb)*((i+1)/2)), &wg_sobel)
+  }
+  wg_sobel.Wait()
+  max_value := float64(0)
+  for y := 0; y<height; y++{
+    for x := width_deb; x<width_fin; x++{
+      valX := sumX[y][x]
+      valY := sumY[y][x]
+      gradiant := float64(math.Sqrt(valX*valX + valY*valY))
+      if gradiant > max_value {
+        max_value = gradiant
+      }
+      result[y][x] = gradiant
+    } 
+  }
+  for i := 0; i < len(maxChan); i++ {
+    if maxChan[i] == -1 {
+      maxChan[i] = max_value
+    }
+  }
+  defer wg.Done()
+}
+
+func contouring(matrix [][]float64, result [][]float64, lowerThreshold, highThreshold, max_value, tolerance float64, width_deb, width_fin int, wg *sync.WaitGroup) {
+	// Initialisation des constantes
+	height := len(matrix)
+
+	// Parcours de l'image pixel par pixel et comparaison avec les threshold
+	for i := 1; i < height-1; i++ {
+		for j := width_deb; j < width_fin+1; j++ {
+
+			// comparaison avec les thresholds + suppression non max
+			if matrix[i][j] < lowerThreshold || matrix[i][j] < (max_value*(1-tolerance)) {
+				result[i][j] = 0
+
+			} else if matrix[i][j] > highThreshold {
+				result[i][j] = 255
+
+			} else {
+				// Cas ou on est entre les threshold, on vérifie la présence de pixels forts voisins
+				for k := -1; k <= 1; k++ {
+					for l := -1; l <= 1; l++ {
+						if matrix[i+k][j+l] > highThreshold {
+							result[i][j] = 255
+						}
+					}
+				}
+				if matrix[i][j] != 255 {
+					result[i][j] = 0
+				}
+			}
+		}
+	}
+	defer wg.Done()
+}
+
+func read_img(imageFournie string) image.Image {
+	// Ouvrir le fichier image
+	imageFile, err := os.Open(imageFournie)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer imageFile.Close() //On attends qu'open finisse de s'exécuter avant de close
+
+	// Décode l'image
+	img, _, err := image.Decode(imageFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return img
+}
+
+func write_img(img string, matrix [][]float64) {
+
+	grayImage := image.NewGray(image.Rect(0, 0, len(matrix[0]), len(matrix)))
+
+	// Parcours de la matrice et assignation des niveaux de gris à l'image
+	for y := 0; y < len(matrix); y++ {
+		for x := 0; x < len(matrix[0]); x++ {
+			grayValue := matrix[y][x]
+			var grayValue2 uint8 = uint8(math.Round(float64(grayValue)))
+			grayImage.SetGray(x, y, color.Gray{Y: grayValue2})
+		}
+	}
+
+	// Création d'un fichier image PNG
+	file, err := os.Create(img)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Encodage de l'image en format PNG et écriture dans le fichier
+	if err := png.Encode(file, grayImage); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func rgb_to_grayscale_paral(img image.Image, grayMatrix [][]float64, height, width_deb, width_fin int, wg *sync.WaitGroup) {
+
+  defer wg.Done()
+	//conversion en gray scale using 0.299 ∙ Red + 0.587 ∙ Green + 0.114 ∙ Blue formula per pixel
+
+	const cstred float64 = 0.299   //constante red
+	const cstgreen float64 = 0.587 //constante green
+	const cstblue float64 = 0.114  //constante blue
+
+	// Parcours de tous les pixels de l'image et stockage des valeurs R, G, B dans la matrice
+	for y := 0; y < height; y++ {
+		for x := width_deb; x < width_fin; x++ {
+			r, g, b, _ := img.At(x, y).RGBA()
+			grayMatrix[y][x] = cstred*float64(r>>8) + cstgreen*float64(g>>8) + cstblue*float64(b>>8)
+
+		}
+	}
+}
+
+func main() {
+	start := time.Now()
+
+	// Gestion d'images
+	img := read_img("input/wall_anime_8K.png")
+
+	bounds := img.Bounds()
+	width, height := bounds.Max.X, bounds.Max.Y
+
+	const cstred float64 = 0.299   //constante red pour la norme grayscale
+	const cstgreen float64 = 0.587 //constante green pour la norme grayscale
+	const cstblue float64 = 0.114  //constante blue pour la norme grayscale
+
+	// Paramètres de fonctions
+	kernelSize := 5
+	sigma := 1.4
+	tolerance := 0.8
+	minThreshold := 0.1
+	maxThreshold := 0.4
+
+	// Elements fixes de fonctions
+	kernel := generateGaussianFilter(kernelSize, sigma)
+	max_valueChan := []float64{-1, -1}
+
+	// Matrices d'écriture
+	grayMatrix := make([][]float64, height)
+	imgGauss := make([][]float64, height)
+	imgRes := make([][]float64, height+2)
+	imgGrad := make([][]float64, height+2)
+
+	// Initalisation des matrices
+	for i := 0; i < height; i++ {
+		grayMatrix[i] = make([]float64, width)
+		imgGauss[i] = make([]float64, width)
+	}
+	for j := 0; j < height+2; j++ {
+		imgGrad[j] = make([]float64, width+2)
+		imgRes[j] = make([]float64, width+2)
+	}
+	// Variables de threads
+	threads := float64(8)
+	var wg sync.WaitGroup
+
+	// Use of worker
+	for i := float64(0); i < threads; i++ {
+		wg.Add(1)
+    go rgb_to_grayscale_paral(img, grayMatrix, int(float64(height)), int(float64(width)*(i/threads)), int(float64(width)*((i+1)/threads)), &wg)
+	}
+	// Need to wait for full image
+	wg.Wait()
+
+	for i := float64(0); i < threads; i++ {
+		wg.Add(1)
+		go convolveParal(grayMatrix, imgGauss, kernel, int(float64(width)*(i/threads)), int(float64(width)*((i+1)/threads)), &wg)
+	}
+	wg.Wait()
+
+	for i := float64(0); i < threads; i++ {
+		wg.Add(1)
+		go sobel(imgGauss, imgGrad, int(float64(width)*(i/threads)), int(float64(width)*((i+1)/threads)), max_valueChan, &wg)
+	}
+	wg.Wait()
+
+
+
+	// Recupération de la valeur maximale
+	max_value := max_valueChan[0]
+	for _, value := range max_valueChan {
+		if value > max_value {
+			max_value = value
+		}
+	}
+
+	for i := float64(0); i < threads; i++ {
+		wg.Add(1)
+		go contouring(imgGrad, imgRes, minThreshold, maxThreshold, max_value, tolerance, int(float64(width)*(i/threads)), int(float64(width)*((i+1)/threads)), &wg)
+	}
+	wg.Wait()
+
+	//Image writing
+	write_img("output/output.png", imgRes)
+
+	// See time
+	elapsed := time.Since(start)
+	fmt.Printf("c'est bon : %s\n", elapsed)
+}
---
+++ JS/jarnac.js
@@ -0,0 +1,149 @@
+const { writeFile, appendFile } = require('node:fs/promises');
+
+let grilleA = ["","","","","","","","test"];
+let grilleB = ["","","","","","","chose",""];
+
+function draw(sac, n) {
+  let letters = [];
+  for (let i = 0; i < n; i++) {
+    const element = n[i];
+    let nb_aleat = Math.floor(Math.random()*26);
+    while (sac[nb_aleat] == 0) { 
+      let nb_aleat = Math.floor(Math.random()*26);
+    }
+    letters.push(nb_aleat);
+    sac[nb_aleat] -= 1;
+  }
+  return letters
+}
+
+function fileInit(file) {
+  try {
+    writeFile(file, "");
+    console.log("c'est bongue");
+  } catch (error) {
+    console.log("erreur", error.message);
+  };
+}
+
+function log(file, data) {
+  try {
+    appendFile(file, data+"\n\r");
+  } catch (error) {
+    console.log("error", error.message)
+  }
+}
+
+function anagram(newWord,word,carpet) {
+  // Initialisation
+  const res = true
+  newWord = newWord.toUpperCase()
+  word = word.toUpperCase()
+
+  // Test de la présence du mots dans la nouvelle propal
+  for (let i = 0; i < word.length; i++) {
+    if (!newWord.includes(word[i])){
+      res = false
+    };
+  }
+  // Test si les lettres restantes sont sur le tapis
+  remains = newWord.replace(word,'')
+  for(let i=0;i<6;i++) {
+    const letter = carpet[i];
+    if (remains.includes(letter)) {remains = remains.replace(letter,'')}
+  }
+  if (remains === '') {res = true}
+  
+  return res
+}
+function validstr(word){
+  let valid = false;
+  if (word.length>9){
+    console.log("le mot ne doit pas dépasser 9 caractères")
+  }
+  else{
+    valid = true;
+  }
+  return valid
+}
+
+function pointsCounter(grille){
+  let points = 0;
+  for(let i=0;i<8;i++) {
+    points = points + (grille[i].length)**2
+  }
+  return points
+
+}
+
+function initGame(){
+    // Creation des objets de jeu
+    let sac = [14, 4, 7, 5, 19, 2, 4, 2, 11, 1, 1, 6, 5, 9, 8, 4, 1, 10, 7, 9, 8, 2, 1, 1, 1, 2];
+    let tour = 0;
+    let carpets = [draw(sac, 6),draw(sac,6)];
+    let grilles = [[],[]];
+    for (let i = 0; i < 2; i++){
+      let grilleA = ["","","","","","","",""];
+      let grilleB = ["","","","","","","",""];
+    }
+  
+    return {tour,sac,carpets,grilles}
+}
+
+function game(){
+    // Initialisation du jeu
+    gameValues = initGame()
+    let tour = gameValues.tour
+    let sac = gameValues.sac
+    let carpets = gameValues.carpets
+    let grilles = gameValues.grilles
+    let tourEnCours = 0
+  
+    // Affichage de début de jeu
+    console.log("Let's begin")
+  
+    // Boucle de jeu
+    while (true){
+      if (tour%2==0){
+        console.log("aoiadzjçaodaçadnabd  anoiadnaoiazhuazodbdoabaz")
+      }
+    }
+}
+
+function gameEnd(grilleA, grilleB){
+  if (grilleA[7]!="" || grilleB[7]!=""){
+    let game_over = true;
+    console.log("points de l'équipe A:",pointsCounter(grilleA))
+    console.log("points de l'équipe B:",pointsCounter(grilleB))
+  
+  }
+  else{
+    console.log("le jeu continue")
+  }
+}
+
+let sac = [14, 4, 7, 5, 19, 2, 4, 2, 11, 1, 1, 6, 5, 9, 8, 4, 1, 10, 7, 9, 8, 2, 1, 1, 1, 2];
+let carpet = draw(sac, 5)
+console.log("carpet")
+console.log(carpet)
+console.log(sac)
+console.log(carpet.map((letter) => String.fromCharCode(letter + 65)))
+console.log([19,18,20,17,21].map((letter) => String.fromCharCode(letter + 65)))
+console.log(anagram("ruat","rat",[19,18,20,17,21].map((letter) => String.fromCharCode(letter + 65))))
+gameEnd(grilleA,grilleB)
+
+game()
+
+fileInit('log')
+
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+
+const rl = readline.createInterface({ input, output });
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  log('log', answer)
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
