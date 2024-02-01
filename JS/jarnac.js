f37612eb6aa1bd8a7cc2d6e0f8495f47a843c3bb
Madmuses <mamusique128@gmail.com>
2024-02-01 10:12:53 +0100

    Fonction d'affichage ajoutée

--- JS/jarnac.js
+++ JS/jarnac.js
@@ -79,21 +79,21 @@ function game(){
   let sac = [14, 4, 7, 5, 19, 2, 4, 2, 11, 1, 1, 6, 5, 9, 8, 4, 1, 10, 7, 9, 8, 2, 1, 1, 1, 2];
   let tour = 0;
   let carpets = [draw(sac, 6),draw(sac,6)];
-  console.log("Let's begin")
-  let grilles = [[],[]];
-  for (let i = 0; i < 2; i++){
-    grilles.push(["","","","","","","",""]);
-  }
-
-  console.log(grilles)
+  let grilles = [
+    ["RAT","","","","","","",""],["NO","","","","","","",""]
+  ];
 
   // Variables de tour
-  let joueurTour = 0 // Au début du tour
-  let adversaire = 1
+  let joueur = 0;// Au début du tour
+  let adversaire = 1;
+  let valid = 0;
 
   // Affichage de début de jeu
-  console.log("Let's begin")
+  console.log("Let's begin\n")
 
+  affichage(grilles[joueur],carpets[joueur],joueur)
+
+  /*
   // Boucle de jeu
   while (true){
     if (tour%2==0){
@@ -101,18 +101,47 @@ function game(){
       action = "Jouer".toLowerCase()
       if (action == "jouer"){adversaire = 0}
       else if (action == "jarnac"){adversaire = 1}
-      else {let end = true}
+      else {
+        tour ++
+
+      }
       
-      // Afficher la grille et le tapis
-      // Verifier ou il veut jouer
-      // Proposer les endroits ou il peut jouer et demander ou il joue
+      valid = affichage(grilles[joueur],carpets[joueur],joueur) // Afficher la grille et le tapis
+
+      // Input : Proposer les endroits ou il peut jouer et demander ou il joue
       // Bonus : Decouper les lettres dispos (affichage mais on verra plus tard)
-      // Verifier que l'input est valide (Longueur + rapport au mot + carpet)
+      verified = verifMot()// Verifier que l'input est valide (Longueur + rapport au mot + carpet)
       // Placer le nouveau mot, déduire du tapis les lettres utilisées
 
 
     }
+  }*/
+}
+
+function verifMot(newWord,word,carpet){
+  verified = anagram(newWord,word,carpet)
+}
+
+function affichage(grille,carpet,player){
+  let valid = 0;
+
+  // Affichage du tapis
+  console.log("Tapis du Joueur : "+String(player+1))
+  let strCarpet = ""
+  for (let i=0;i<carpet.length;i++){
+    strCarpet += " " + String.fromCharCode(carpet[i] + 65);
   }
+  console.log(strCarpet + "\n")
+  // Affichage de la grille
+  console.log("Grille du joueur : "+String(player+1))
+  for (let i=0;i<8;i++){
+    if (grille[i]!=""){
+      valid ++;
+      console.log(String(i+1)+". : "+grille[i])
+    }
+  }
+  console.log(String(valid+1)+". : \n")
+  return valid
 }
 
 
@@ -128,19 +157,22 @@ function gameEnd(grilleA, grilleB){
   }
 }
 
-//let sac = [14, 4, 7, 5, 19, 2, 4, 2, 11, 1, 1, 6, 5, 9, 8, 4, 1, 10, 7, 9, 8, 2, 1, 1, 1, 2];
-//let carpet = draw(sac, 5)
-//console.log("carpet")
-//console.log(carpet)
-//console.log(sac)
-//console.log(carpet.map((letter) => String.fromCharCode(letter + 65)))
-//console.log([19,18,20,17,21].map((letter) => String.fromCharCode(letter + 65)))
-//console.log(anagram("ruat","rat",[19,18,20,17,21].map((letter) => String.fromCharCode(letter + 65))))
-
 game()
 
 fileInit('log')
 
+/*
+let sac = [14, 4, 7, 5, 19, 2, 4, 2, 11, 1, 1, 6, 5, 9, 8, 4, 1, 10, 7, 9, 8, 2, 1, 1, 1, 2];
+let carpet = draw(sac, 5)
+console.log("carpet")
+console.log(carpet)
+console.log(sac)
+console.log(carpet.map((letter) => String.fromCharCode(letter + 65)))
+console.log([19,18,20,17,21].map((letter) => String.fromCharCode(letter + 65)))
+console.log(anagram("ruat","rat",[19,18,20,17,21].map((letter) => String.fromCharCode(letter + 65))))
+*/
+
+/*
 const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
 
@@ -152,3 +184,4 @@ rl.question('What do you think of Node.js? ', (answer) => {
 
   rl.close();
 });
+*/
