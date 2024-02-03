47fd80db80b6826de4dbb6db473df661c049f4ba
Madmuses <mamusique128@gmail.com>
2024-02-03 17:57:44 +0100

    now uses register function and has a list of possible words

--- JS/verifs.js
+++ JS/verifs.js
@@ -6,7 +6,9 @@ var contenuFichier;
 
 // Lecture du dico et stockage de son contenu en var globale
 try {
-  contenuFichier = fs.readFileSync('liste_francais.txt', 'utf8');
+  let contenu = fs.readFileSync('liste_francais.txt', 'utf8');
+  contenuFichier = contenu.split("\n")
+  console.log(contenuFichier)
   console.log("Contenu du dictionnaire bien stocké dans la variable globale");
   
 } catch (err) {
@@ -19,19 +21,13 @@ function verifs(){
 
     function validstr(word,joueur){
         if (word.length>9){
-        console.log("le mot ne doit pas dépasser 9 caractères.");
-        file.log("log", "Joueur " + joueur+1 + " : le mot ne doit pas dépasser 9 caractères")
-        .then(() => resolve())
-        .catch((error) => console.log("Erreur lors de l'écriture de log" + error))
-        return false
-        }
+            console.log("le mot ne doit pas dépasser 9 caractères.");
+            file.register("Joueur " + joueur+1 + " : le mot ne doit pas dépasser 9 caractères")
+            return false}
         else if (word.length < 3){
-        console.log("Le mot doit être supérieur à trois caractères.");
-        file.log("log", "Joueur " + joueur+1 + " : le mot doit être supérieur à trois caractères")
-        .then(() => resolve())
-        .catch((error) => console.log("Erreur lors de l'écriture de log" + error))
-        return false
-        }
+            console.log("Le mot doit être supérieur à trois caractères.");
+            file.register("Joueur " + joueur+1 + " : le mot doit être supérieur à trois caractères")
+            return false}
         return true;
     }
   
@@ -41,9 +37,7 @@ function verifs(){
     word = word.toUpperCase()
     if (word.length >= newWord.length) {
       console.log("Il faut jouer toutes les lettres du mot présent sur la grille et en rajouter depuis le tapis")
-      file.log("log", "Le mot joué n'ajoute pas de lettres par rapport au mot déjà présent sur la grille")
-      .then(() => resolve())
-      .catch((error) => console.log("Erreur lors de l'écriture de log" + error))
+      file.register("Le mot joué n'ajoute pas de lettres par rapport au mot déjà présent sur la grille")
       return false
     }
     // Test de la présence du mots dans la nouvelle propal
@@ -71,20 +65,15 @@ function verifs(){
 
         if (contenuFichier.includes(mot)) {
             console.log("Le mot est bien présent dans le dictionnaire");
-        file.log("log", "Le mot est bien présent dans le dictionnaire")
-        .then(() => resolve())
-        .catch((error) => console.log("Erreur lors de l'écriture de log" + error))
+            file.register("Le mot est bien présent dans le dictionnaire")
             return true;
         }
         console.log("Le mot n'est pas présent dans le dictionnaire");
-        file.log("log", "Le mot n'est pas présent dans le dictionnaire")
-        .then(() => resolve())
-        .catch((error) => console.log("Erreur lors de l'écriture de log" + error))
-        avis = file.input("Acceptez-vous tout de meme ce mot ?(oui/non) ",["oui","non"]);
-        if (avis==="oui"){
-          file.log("log", "Le mot est tout de même accepté par le joueur")
-          .then(() => resolve())
-          .catch((error) => console.log("Erreur lors de l'écriture de log" + error))
+        file.register("Le mot n'est pas présent dans le dictionnaire")
+
+        avis = file.input("Acceptez-vous tout de meme ce mot ?(oui/non) ",["oui","non","o","n"]);
+        if (avis==="oui" || avis==="o"){
+          file.register("Le mot est tout de même accepté par le joueur")
           return true;
         };
         return false
